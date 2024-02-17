f69103e6055562d5756a4b396ecd51f1ead41260
Moises Felix <moises012felix@gmail.com>
2024-02-17 11:35:41 -0300

    Enviando projeto produto feito em POO

---
+++ src/application/Program.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//Product p = new Product();
+		
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.println("Price: ");
+		double price = sc.nextDouble();
+		
+		Product product = new Product(name, price);
+		
+		System.out.println();
+		System.out.println("product data: " +product.toString());
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in Stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " +product.toString());
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from Stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " +product.toString());
+		
+		sc.close();
+	}
+
+}
